ff0118eb1553f0304db56cf3e8c415b8c711e59b
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-04 16:32:35 +0300

    ft: add admins dashboard

--- Instagram/admin.py
+++ Instagram/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Comment, Post, Like
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class StyledComment(UserAdmin):
+    list_display = ('post','user','body', 'date_posted')
+    filter_horizontal = ()
+    fieldsets = ()
+    ordering = ('-post',)
+    list_filter = ()
+
+
+
+admin.site.register(Comment, StyledComment)
+admin.site.register(Post, StylePost)
+admin.site.register(Like)
