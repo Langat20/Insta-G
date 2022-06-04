557030ee3b7b9f12692a5f5f07e8b791f085b39c
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-04 16:24:27 +0300

    ft : create class model post

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import UserAccount
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Post(models.Model):
+    host = models.ForeignKey(UserAccount, on_delete=models.SET_NULL, null=True)
+    image = CloudinaryField("image")
+    description = models.CharField(max_length=100)
+    liked = models.ManyToManyField(UserAccount, blank=True, null=True, related_name='liked')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
+
+
